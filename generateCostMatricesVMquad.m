8cfd6d7695c124eb74b4f6aab0ffaa8d08ed0ed3
nickMont <nick30075@utexas.edu>
2020-06-29 14:45:45 -0500

    missing files

---
+++ generateCostMatricesVMquad.m
@@ -0,0 +1,113 @@
+function [Jpur,Jeva,uPcell,uEcell] = generateCostMatricesVMquad(Spur, Seva, gameState,vk)
+%inputs:
+%     structures Spur,Seva containing:
+%        uMat   %Possible control value structure. uMat{ik} contains the
+%                nUxK matrix of controls of the ik^th action to be applied
+%                at time instant K
+%        Jname  %string function name for cost function
+%        fname  %string dynamics function name
+%        Jparams  %struct containing parameters for J    
+%        
+%
+%     structure gameState containing:
+%        xPur, xEva  %states of pursuer and evader
+%        dt  %dt between steps
+%        kMax  %number of time steps to plan over
+%
+%
+%   Dynamics function f_dyn is referenced.  It can be changed if dynamics
+%       are also changed. Likewise for Jpur(xpur,xeva,upur,ueva)
+
+if ~isfield(Spur,'UseVelMatch') || ~isfield(Seva,'UseVelMatch')
+    error('HARDCODED TO ONLY HAND VM/GT HYBRID--Have you added a UseVelMatch flag to Spur/Seva?')
+elseif ~Spur.UseVelMatch || ~Seva.UseVelMatch
+    error('UseVelMatch set to false but generateCostMatricesVM2 was called')
+end
+
+nmodP = length(Spur.uMat);
+nmodE = length(Seva.uMat);
+
+Jpur=9001*ones(nmodP,nmodE);
+Jeva=42*ones(nmodP,nmodE);
+
+nx = length(gameState.xPur);
+% state = zeros(nx,gameState.kMax+1);
+xPurCell = cell(nmodP,nmodE);
+xEvaCell = cell(nmodP,nmodE);
+uPcell = cell(nmodP,nmodE);
+uEcell = cell(nmodP,nmodE);
+for ij=1:nmodP
+    for iL=1:nmodE
+        stateP(:,1)=gameState.xPur;
+        stateE(:,1)=gameState.xEva;
+        uPm=[];
+        uEm=[];
+        for ik=1:gameState.kMax
+            xp=stateP([7 8 10 11],ik);
+            xe=stateE([7 8 10 11],ik);
+            if gameState.controlType=='vmquad'
+                [uhatVelMatch,states] = vmRGVO_tune(xp,xe,gameState.uMaxP,2,gameState.dt,zeros(2,1),1);
+                uhatVelMatch = unit_vector(uhatVelMatch);
+                %limit vector to quadrants 1+4
+                theta=atan2(uhatVelMatch(2),uhatVelMatch(1));
+                if theta>pi/2 && theta<3*pi/2
+                    uhatVelMatch=-uhatVelMatch;
+                end
+                xPt=states.xPout;
+                xEt=states.xEout;
+                
+                uPtilde=uhatVelMatch*norm(Spur.uMat{ij}(:,ik));
+                uEtilde=uhatVelMatch*norm(Seva.uMat{iL}(:,ik));
+                %update states here
+                
+                xd = stateP(:,ik); xd(9)=0;
+                uP = quadControllerACCONLY(xd, zeros(4,1), 3, [uPtilde;0],0);
+                xd = stateE(:,ik); xd(9)=0;
+                uE = quadControllerACCONLY(xd, zeros(4,1), 3, [uEtilde;0],0);
+            elseif
+                
+            end
+            
+            %P
+            stateP(:,ik+1) = feval(Spur.fname,stateP(:,ik),uP,gameState.dt,zeros(6,1));
+            stateE(:,ik+1) = feval(Spur.fname,stateE(:,ik),uE,gameState.dt,zeros(6,1));
+            uPm=[uPm uP];
+            uEm=[uEm uE];
+        end
+        uPcell{ij,iL} = uPm;
+        uEcell{ij,iL} = uEm;
+        xPurCell{ij,iL} = stateP;
+        xEvaCell{ij,iL} = stateE;
+    end
+end
+
+% nx = length(gameState.xEva);
+% state1 = zeros(nx,gameState.kMax+1);
+% 
+% for ij=1:nmodE
+%     state(:,1)=gameState.xEva;
+%     for ik=1:gameState.kMax
+%         if isfield(Seva,'params')
+%             state(:,ik+1) = feval(Seva.fname,state(:,ik),Seva.uMat{ij}(:,ik),gameState.dt,zeros(nv,1),Seva.params);
+%         else
+%             state(:,ik+1) = feval(Seva.fname,state(:,ik),Seva.uMat{ij}(:,ik),gameState.dt,zeros(nv,1));
+%         end
+%     end
+%     xEvaCell{ij} = state;
+% end
+
+noiseCostPur=3409;
+noiseCostEva=3409;
+Jpur=zeros(nmodP,nmodE);
+Jeva=zeros(nmodP,nmodE);
+for iP=1:nmodP
+    for iE=1:nmodE
+        Jpur(iP,iE)=feval(Spur.Jname,xPurCell{iP,iE},xEvaCell{iP,iE},uPcell{iP,iE},uEcell{iP,iE},Spur.Jparams);
+        Jeva(iP,iE)=feval(Seva.Jname,xEvaCell{iP,iE},xPurCell{iP,iE},uEcell{iP,iE},uPcell{iP,iE},Seva.Jparams);
+    end
+end
+
+
+
+end
+
---
+++ topSimulateQuadPE_VMGT.m
@@ -0,0 +1,133 @@
+clear;clc;
+beep off;
+
+%NOTE: TUNING IN VMRGO REQUIRES USING fdynEva AND fdynPur FOR PM SIM
+%  SET DRAG PARAMS IN BOTH TO 0.
+%NOTE: set the target function to generateCostMatricesVMquad in f_dyn2
+%  BEFORE running this; hardcoded the other way to remain backwards
+%  compatible
+
+% Jp=1.0408e+04
+% Je=.3878e+03
+
+umax=0.1;
+
+dt=0.1;
+t0=0;
+tmax=10;
+
+%utemp=permn(-2:0.2:2,2)';
+uvec=-1:0.2:1;
+utemp=permn(uvec,2)';
+upmax=umax;
+umax=upmax;
+utype.radius=upmax;
+utemp=mapUtempToUvec(utemp,"circle",utype);
+
+n=0;
+z31=zeros(3,1);
+ewxvPur=[z31; z31; 1;0.1;0; z31];
+ewxvEva=[zeros(12,1)];
+xPur=[ewxvPur;ewxvEva];
+xEva=[ewxvPur;ewxvEva];
+xTrue=xPur;
+
+Qpur=zeros(12,12);
+Qpur(7:9,7:9)=5*eye(3);
+Qeva=zeros(12,12);
+Qeva(7:9,7:9)=5*eye(3);
+Rpur=eye(4);
+Reva=eye(4);
+xStore=xPur;
+
+uPhist=[];
+uEhist=[];
+
+JJp=0;
+JJe=0;
+
+tic
+for t=t0:dt:tmax
+    n=n+1;
+    
+%     upurset=umin:du:umax;
+%     %uPur=combvec(upurset,upurset,upurset,upurset);
+%     uPur=combvec(upurset,upurset,upurset);
+%     uEva=uPur;
+    
+    % Guessing pursuer
+    gameState_p.xPur=xPur(1:12);
+    gameState_p.xEva=xPur(13:24);
+    gameState_p.dt=dt;
+    gameState_p.kMax=1;
+    gameState_p.nu=2;
+    gameState_p.discType='overX';
+    gameState_p.uMaxP=umax;
+    for ik=1:length(uvec)
+        Spur_p.uMat{ik}=upmax*uvec(ik);
+    end
+    Spur_p.Jname='J_purQuad';
+    Spur_p.fname='f_dynPurQuad';
+    Spur_p.Jparams.Q=Qpur;
+    Spur_p.Jparams.Rself=Rpur;
+    Spur_p.Jparams.Ropp=zeros(4,4);
+    Spur_p.UseVelMatch=true;
+    for ik=1:length(uvec)
+        Seva_p.uMat{ik}=Spur_p.uMat{ik};
+    end
+    Seva_p.Jname='J_evaQuad';
+    Seva_p.fname='f_dynEvaQuad';
+    Seva_p.Jparams.Q=Qeva;
+    Seva_p.Jparams.Rself=Reva;
+    Seva_p.Jparams.Ropp=zeros(4,4);
+    Seva_p.UseVelMatch=true;
+    % Propagate to next time step
+    [up,ue,flag,uPSampled,uESampled]=f_dyn2(Spur_p,Seva_p,gameState_p,zeros(4,1));
+    uPurTrue=uPSampled;
+    uEvaTrue=uESampled;
+    
+    uPhist=[uPhist uPurTrue];
+    uEhist=[uEhist uEvaTrue];
+    xTrue(1:12)=f_dynPurQuad(xTrue(1:12),uPurTrue(:,1),dt,zeros(2,1));
+    xTrue(13:24)=f_dynEvaQuad(xTrue(13:24),uEvaTrue(:,1),dt,zeros(2,1));
+    xD=[zeros(24,1) xTrue];
+    
+    Jpur=feval(Spur_p.Jname,xD(1:12,:),xD(13:24,:),uPurTrue,uEvaTrue,Spur_p.Jparams);
+    Jeva=feval(Seva_p.Jname,xD(13:24,:),xD(1:12,:),uEvaTrue,uPurTrue,Seva_p.Jparams);
+    JJp=JJp+Jpur
+    JJe=JJe+Jeva
+    
+    noise=zeros(24,1);
+    xEva=xTrue+noise;
+    xPur=xTrue+noise;
+    
+    xStore=[xStore xTrue];
+end
+tTotal=toc
+
+%     load quaddat.mat;
+indsamp=1:5:100;
+xP2d=xStore(7:8,indsamp);
+xE2d=xStore(19:20,indsamp);
+% mx=(xP2d(2,1)-xE2d(2,1))/(xP2d(1,1)-xE2d(1,1));
+% xPlin_x=-5:0.3:1;
+% xPlin_y=xPlin_x*mx;
+% xElin_x=-6:0.3:0;
+% xElin_y=xElin_x*mx;
+
+figure(1);clf;
+figset
+plot(xP2d(1,:),xP2d(2,:),'-*b');
+hold on
+plot(xE2d(1,:),xE2d(2,:),'-Or');
+% hold on
+% plot(xPlinCompare(1,:),xPlinCompare(2,:),'-.*k');
+% hold on
+% plot(xElinCompare(1,:),xElinCompare(2,:),'-.og');
+axis([-6 2 -6 2])
+xlabel('x-position (m)')
+ylabel('y-position (m)')
+legend('Pursuer trajectory, quad dynamics','Evader trajectory, quad dynamics','Pursuer trajectory, point mass','Evader trajectory, point mass');
+figset
+
+
